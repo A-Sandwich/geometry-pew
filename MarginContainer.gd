269f3f3b93b2257208295824b8c9c27fa3596c96
Kyle <kbburkholder@gmail.com>
2020-07-04 12:49:25 -0400

    Controls screen works, fixed main menu

--- MarginContainer.gd
+++ MarginContainer.gd
@@ -5,7 +5,6 @@ onready var COMMON = get_node("/root/Common")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var player = $VBoxContainer/Player
-	$VBoxContainer/Player/ThrustParticleFast.visible = false
 	COMMON.thrust($VBoxContainer/Player/ThrustParticle, Vector2(1,0), player.sprite_width, player.position)
 
 func _on_StartGame_pressed():
@@ -15,4 +14,4 @@ func _on_Exit_pressed():
 	get_tree().quit()
 
 func _on_Start_pressed():
-	get_tree().change_scene("res://Stage.tscn")
+	get_tree().change_scene("res://UI/Controls.tscn")
--- UI/Controls.gd
+++ UI/Controls.gd
@@ -5,17 +5,24 @@ var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var controller = $UI/HBoxContainer/XboxController
-	var wasd = $UI/HBoxContainer/VBoxContainer/WASD
-	var arrows = $UI/HBoxContainer/VBoxContainer/Arrows
+	var controls = $UI/Controls
 	screen_size = get_viewport_rect().size
-	var size_constraint = screen_size.x / 2
-	var controller_size = controller.get_rect().size
-	var scale = size_constraint / controller_size.x
-	controller.scale = Vector2(scale, scale)
-	wasd.scale = Vector2(scale, scale)
-	arrows.scale = Vector2(scale, scale)
-	print(controller_size)
-	controller.position = Vector2((controller_size.x * scale) / 2,  (controller_size.y * scale))
-	wasd.position = Vector2(controller.position.x + ((wasd.get_rect().size.x * scale)), (wasd.get_rect().size.y * scale) / 2)
-	arrows.position = Vector2(controller.position.x + ((arrows.get_rect().size.x * scale)), (arrows.get_rect().size.y * scale) + wasd.position.y)	
+	var size_constraint = screen_size.x
+	var controls_size = controls.get_rect().size
+	var scale = size_constraint / controls_size.x
+	controls.scale = Vector2(scale, scale)
+	controls.position = Vector2((controls_size.x * scale) / 2,  (controls_size.y * scale) / 2)
+
+func _process(delta):
+	# wow, such beauty
+	if (Input.is_action_just_pressed("bomb") or 
+	 Input.is_action_just_pressed("thrust") or
+	 Input.is_action_just_pressed("up") or
+	 Input.is_action_just_pressed("down") or
+	 Input.is_action_just_pressed("left") or
+	 Input.is_action_just_pressed("right") or
+	 Input.is_action_just_pressed("fire_up") or
+	 Input.is_action_just_pressed("fire_down") or
+	 Input.is_action_just_pressed("fire_left") or
+	 Input.is_action_just_pressed("fire_right")):
+		get_tree().change_scene("res://Stage.tscn")
