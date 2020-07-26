9a6e5c24bd45a38ede83972cf9c9114c927b8221
Kyle <kbburkholder@gmail.com>
2020-07-26 11:01:04 -0400

    Added keyboard navigation to the main menu

--- MarginContainer.gd
+++ MarginContainer.gd
@@ -2,6 +2,10 @@ extends MarginContainer
 
 onready var COMMON = get_node("/root/Common")
 var screen_size
+const DEFAULT_COLOR = Color(1, 1, 1, 0.8)
+const HOVER_COLOR = Color(1, 1, 1, 1)
+var focus
+var ui_elements = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,24 +14,59 @@ func _ready():
 	COMMON.thrust($VBoxContainer/Player/ThrustParticle, Vector2(1,0), player.sprite_width, player.position)
 	screen_size = player.screen_size
 	rect_size = screen_size
-	position_particle($VBoxContainer/Start/StartParticle, $VBoxContainer/Start)
-	position_particle($VBoxContainer/Options/OptionsParticle, $VBoxContainer/Options)
-	position_particle($VBoxContainer/Credits/CreditsParticle, $VBoxContainer/Credits)
-	position_particle($VBoxContainer/Exit/ExitParticle, $VBoxContainer/Exit)
+	setup_buttons()
 	var ui_height = $VBoxContainer/Start.rect_size.y * 4	
 	$CanvasLayer/Version.rect_position = Vector2(0, screen_size.y - $CanvasLayer/Version.rect_size.y)
 	update()
 
+func _process(delta):
+	if Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_focus_prev"):
+		var index = ui_elements.find(focus)
+		if index <= 0:
+			pass
+		else:
+			on_entered(ui_elements[index - 1])
+	elif Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("ui_focus_next"):
+		var index = ui_elements.find(focus)
+		if index == -1:
+			on_entered(ui_elements[0])
+		elif index == (len(ui_elements) - 1):
+			pass
+		else:
+			on_entered(ui_elements[index + 1])
+	elif Input.is_action_just_pressed("ui_accept"):
+		if focus != null:
+			focus.emit_signal("pressed")
+
+func setup_buttons():
+	position_particle($VBoxContainer/Start/StartParticle, $VBoxContainer/Start)
+	position_particle($VBoxContainer/Options/OptionsParticle, $VBoxContainer/Options)
+	position_particle($VBoxContainer/Credits/CreditsParticle, $VBoxContainer/Credits)
+	position_particle($VBoxContainer/Exit/ExitParticle, $VBoxContainer/Exit)
+	for child in $VBoxContainer.get_children():
+		if child is Button:
+			child.modulate = DEFAULT_COLOR
+			ui_elements.append(child)
 func position_particle(particle, parent):
 	var particle_position = Vector2(screen_size.x/2, parent.rect_size.y / 2)
 	particle.position = particle_position
 	particle.update()
 
+func on_entered(button):
+	if focus != null and button != focus:
+		on_exited(focus)
+	focus = button
+	button.modulate = HOVER_COLOR
+	button.get_children()[0].emitting = true
+	
+func on_exited(button):
+	button.modulate = DEFAULT_COLOR
+	button.get_children()[0].emitting = false
+
 func _on_StartGame_pressed():
 	get_tree().change_scene("res://Stage.tscn")
 
 func _on_Exit_pressed():
-	print("Exit")
 	get_tree().quit()
 
 func _on_Start_pressed():
@@ -35,19 +74,19 @@ func _on_Start_pressed():
 
 
 func _on_Start_mouse_entered():
-	$VBoxContainer/Start/StartParticle.emitting = true
+	on_entered($VBoxContainer/Start)
 
 
 func _on_Start_mouse_exited():
-	$VBoxContainer/Start/StartParticle.emitting = false
+	on_exited($VBoxContainer/Start)
 
 
 func _on_Exit_mouse_entered():
-	$VBoxContainer/Exit/ExitParticle.emitting = true
+	on_entered($VBoxContainer/Exit)
 
 
 func _on_Exit_mouse_exited():
-	$VBoxContainer/Exit/ExitParticle.emitting = false
+	on_exited($VBoxContainer/Exit)
 
 
 func _on_Options_pressed():
@@ -55,11 +94,11 @@ func _on_Options_pressed():
 
 
 func _on_Options_mouse_entered():
-	$VBoxContainer/Options/OptionsParticle.emitting = true
+	on_entered($VBoxContainer/Options)
 
 
 func _on_Options_mouse_exited():
-	$VBoxContainer/Options/OptionsParticle.emitting = false
+	on_exited($VBoxContainer/Options)
 
 
 func _on_Credits_pressed():
@@ -67,8 +106,8 @@ func _on_Credits_pressed():
 
 
 func _on_Credits_mouse_entered():
-	$VBoxContainer/Credits/CreditsParticle.emitting = true
+	on_entered($VBoxContainer/Credits)
 
 
 func _on_Credits_mouse_exited():
-	$VBoxContainer/Credits/CreditsParticle.emitting = false
+	on_exited($VBoxContainer/Credits)
